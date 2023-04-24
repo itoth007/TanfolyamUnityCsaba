0423ddd5e7697108f0a5c4e3d15b8d8aefdb455e
itoth007 <itoth007@gmail.com>
2023-04-24 18:32:50 +0200

    Camera_follower

---
+++ Assets/3D/Camera_follower.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follower : MonoBehaviour
+{
+    [SerializeField] float speed = 5; //speed = player's speed
+    [SerializeField] Transform target; // Drag and drop, whom follows
+    [SerializeField] float distance = 5;
+    [SerializeField] float cameraOffsetY = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 selfPoint = transform.position; //Camera - follower
+        Vector3 targetPoint = target.position; // Player
+
+        Vector3 dir = targetPoint - selfPoint;
+        if (dir != Vector3.zero)
+        {
+            if (dir.magnitude > distance)
+            {
+                transform.rotation = Quaternion.LookRotation(dir); // nem kell normalizálni
+                transform.position = Vector3.MoveTowards(selfPoint, targetPoint+new Vector3(0, cameraOffsetY, 0), speed * Time.deltaTime);
+            }
+        }
+    }
+}
