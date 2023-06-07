877d31813c09c14add5c0374b6d8638b622f6439
itoth07 <itoth007@gmail.com>
2023-06-07 10:00:42 +0200

    sugar2

--- Assets/Scenes/sugar2.cs
+++ Assets/Scenes/sugar2.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class sugar2 : MonoBehaviour
 {
     [SerializeField] GameObject prototype;
-    [SerializeField] float distance;
-    [SerializeField] float lengthRay;
+    [SerializeField] float distance = 1;
+    [SerializeField] float lengthRay = 12;
     [SerializeField] LayerMask mask;
     Vector3 smallRayVector;
     Vector3 cubeDistanceVector;
@@ -16,46 +16,51 @@ public class sugar2 : MonoBehaviour
     //bool wasHit = false;
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
-        Vector3 nowPosition = transform.position;       // A sugár kezdõpontja
-        Vector3 direction = transform.up;     // A sugár iránya
-        Ray ray = new(transform.position, transform.up);          // Sugár
-
-        bool hit = Physics.Raycast(ray, out RaycastHit hitInfo, lengthRay, mask);   // A raycast elvégzése
-        if(prevPosition!=nowPosition || prevRotation!= )
-        //if (hit && !wasHit)
+        Vector3 nowPosition = transform.position + 3 * transform.up;       // A sugár kezdõpontja az ágyu vége
+        Quaternion nowRotation = transform.rotation;
+        if (prevPosition != nowPosition || prevRotation != nowRotation)
         {
-            cubeDistanceVector = (hitInfo.point - transform.position);
-            smallRayVector = (hitInfo.point - transform.position).normalized * distance;
-            //Debug.Log("kis vektor" + );
-            int i = 0;
-            for (int j = 0; j < objects.Count; j++)
+            Ray ray = new(nowPosition, transform.up);          // Sugár
+            bool hit = Physics.Raycast(ray, out RaycastHit hitInfo, lengthRay, mask);   // A raycast elvégzése
+            if (hit)
             {
-                Destroy(objects[j]);
+                cubeDistanceVector = hitInfo.point - nowPosition;
+                smallRayVector = cubeDistanceVector.normalized * distance;
+                ObjectDestroy();
+                int i = 0;
+                while ((smallRayVector * (i + 1)).magnitude < cubeDistanceVector.magnitude)
+                {
+                    objects.Add(Instantiate(prototype));
+                    objects[i].transform.position = nowPosition + smallRayVector * (i + 1);
+                    i++;
+                }
             }
-            objects.Clear();
-            while ((smallRayVector * (i + 1)).sqrMagnitude < cubeDistanceVector.sqrMagnitude)
+            else
             {
-                GameObject globe;
-                globe = Instantiate(prototype, transform.position + smallRayVector * (i + 1), Quaternion.identity);
-                objects.Add(globe);
-                i++;
+                ObjectDestroy();
             }
-            //wasHit = true;
+            prevPosition = nowPosition;
+            prevRotation = nowRotation;
         }
-        else
+    }
+    private void ObjectDestroy()
+    {
+        for (int j = 0; j < objects.Count; j++)
         {
-            //wasHit = false;
+            Destroy(objects[j]);
         }
+        objects.Clear();
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Vector3 rayTo = transform.position + transform.up * lengthRay;
-        Gizmos.DrawLine(transform.position, rayTo);
+        Vector3 rayTo = transform.position + 3 * transform.up + transform.up * lengthRay;
+        Gizmos.DrawLine(transform.position + 3 * transform.up, rayTo);
+        Gizmos.DrawSphere(transform.position + 3 * transform.up, 0.1f);
     }
 }
